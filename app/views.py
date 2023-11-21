5680eda87e57e23c2f29c78b00f3f49c62f12219
BARATH-20002001 <bharathsarangan1@gmail.com>
2023-11-21 14:36:32 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bharath1(request):
+    return render(request,'bharath1.html')
+
+def bharath2(request):
+    return render(request,'bharath2.html')
